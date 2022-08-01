a6b25f494e4390eb53e03df24cb0303022c84692
alexander.baz <alexander.baz@okwork.io>
2022-08-01 20:13:28 +0300

    dev

--- tasks/Unapproved.js
+++ tasks/Unapproved.js
@@ -12,9 +12,7 @@ class Unapproved extends BaseCommand {
   perform = () => {
     return this.projects
       .then(projects => Promise.all(projects.map(this.__getUnapprovedRequests)))
-      .then(requests => {
-        return requests.flat().sort((a, b) => new Date(a.updated_at) - new Date(b.updated_at))
-      })
+      .then(this.__sortRequests)
       .then(this.__buildMessage)
       .then(message => {
         this.logger.info("Sending message")
@@ -34,44 +32,93 @@ class Unapproved extends BaseCommand {
   }
 
   __buildMessage = requests => {
-    const markup = markupUtils[this.config.messenger.markup]
+    const markup = markupUtils[this.__getConfigSetting("messenger.markup")]
 
     if (requests.length) {
-      const list = requests.map(this.__buildRequestDescription).map(markup.addDivider)
-      const headText = "Hey, there are a couple of requests waiting for your review"
-
-      const header = markup.makeHeader(headText)
-      const bodyParts = markup.flatten(list)
-
-      return markup.composeMsg(header, bodyParts)
+      return this.__buildListMessage(requests, markup)
     } else {
-      const headText = "Hey, there is a couple of nothing"
-      const bodyText = "There are no pending requests! Let's do a new one!"
-
-      const header = markup.makeHeader(headText)
-      const body = markup.makePrimaryInfo(markup.makeText(bodyText))
-
-      return markup.composeMsg(header, body)
+      return this.__buildEmptyListMessage(requests, markup)
     }
   }
 
-  __buildRequestDescription = request => {
-    const descriptionBuilder = new UnapprovedRequestDescription(request, this.config)
+  __buildListMessage = (requests, markup) => {
+    const headText = "Hey, there are a couple of requests waiting for your review"
+    const list = this.__buildRequestsMessage(requests, markup)
 
-    return descriptionBuilder.build()
+    const header = markup.makeHeader(headText)
+    const bodyParts = markup.flatten(list)
+
+    return markup.composeMsg(header, bodyParts)
   }
+
+  __buildRequestsMessage = (requests, markup) => {
+    const splitToAndUnderReview =
+      this.__getConfigSetting("unapproved.split_to_and_under_review")
+
+    if (splitToAndUnderReview) {
+      return this.__buildToAndUnderReviewMessage(requests, markup)
+    }
+
+    return this.__buildGeneralRequestsMessage(requests, markup)
+  }
+
+  __buildEmptyListMessage = markup => {
+    const headText = "Hey, there is a couple of nothing"
+    const bodyText = "There are no pending requests! Let's do a new one!"
+
+    const header = markup.makeHeader(headText)
+    const body = markup.makePrimaryInfo(markup.makeText(bodyText))
+
+    return markup.composeMsg(header, body)
+  }
+
+  __buildGeneralRequestsMessage = (requests, markup) => requests
+    .map(this.__buildRequestDescription).map(markup.addDivider)
+
+  __buildToAndUnderReviewMessage = (requests, markup) => {
+    const messages = []
+    const [toReviewRequests, underReviewRequests] = _.partition(requests, req => (
+      req.approvals_left > 0 && !this.__isRequestUnderReview(req)
+    ))
+
+    const makeSection = _.flow(
+      markup.makeBold,
+      markup.makeText,
+      markup.makePrimaryInfo,
+      // x => markup.addDivider([x]),
+    )
+
+    const toReviewSection = makeSection("Unapproved")
+    const underReviewSection = makeSection("Under review")
+
+    const toReviewMessage = this.__buildGeneralRequestsMessage(toReviewRequests, markup)
+    const underReviewMessage = this.__buildGeneralRequestsMessage(underReviewRequests, markup)
+
+    toReviewMessage.length && messages.push(toReviewSection, ...toReviewMessage)
+    underReviewMessage.length && messages.push(underReviewSection, ...underReviewMessage)
+
+    return messages
+  }
+
+  __buildRequestDescription = request =>
+    new UnapprovedRequestDescription(request, this.config).build()
+
+  __sortRequests = requests => requests
+    .flat().sort((a, b) => new Date(a.updated_at) - new Date(b.updated_at))
 
   __getUnapprovedRequests = project => this.__getExtendedRequests(project.id)
     .then(requests => requests.filter(req => {
       const isCompleted = !req.work_in_progress
       const isUnapproved = req.approvals_left > 0
-      const hasUnresolvedDiscussions = req.discussions.some(dis => {
-        return dis.notes.some(note => note.resolvable && !note.resolved)
-      })
+      const isUnderReview = this.__isRequestUnderReview(req)
       const hasPathsChanges = this.__hasPathsChanges(req.changes, project.paths)
 
-      return isCompleted && hasPathsChanges && (isUnapproved || hasUnresolvedDiscussions)
+      return isCompleted && hasPathsChanges && (isUnapproved || isUnderReview)
     }))
+
+  __isRequestUnderReview = req => req.discussions
+    .some(dis => dis.notes
+      .some(note => note.resolvable && !note.resolved))
 
   __hasPathsChanges = (changes, paths) => {
     if (_.isEmpty(paths)) {
@@ -109,6 +156,10 @@ class Unapproved extends BaseCommand {
   __appendDiscussions = (project, request) => this.gitlab
     .discussions(project.id, request.iid)
     .then(discussions => ({ ...request, discussions }))
+
+  __getConfigSetting = (settingName, defaultValue = null) => {
+    return _.get(this.config, settingName, defaultValue)
+  }
 }
 
 module.exports = Unapproved
